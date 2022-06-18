5edfb6dd67efb18a2748f6d4f01c60010d743172
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-06-18 13:02:45 -0500

    Created: Test case for Bubble Sort.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] array = new int[]{0,6,2,4,1,3,7,8};
+        int[] sorted = Algorithms.bubbleSort(array);
+        System.out.println(Arrays.toString(sorted));
     }
 }
