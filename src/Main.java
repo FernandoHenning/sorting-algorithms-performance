b02447941e2eebc9a14f9a35ba5c78a05b1b93d7
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-06-18 19:21:29 -0500

    Created: Test for selection sorting

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = new int[]{0,6,2,4,1,3,7,8};
-        int[] sorted = Algorithms.bubbleSort(array);
-        System.out.println(Arrays.toString(sorted));
+        int[] bubbleSorted = Algorithms.bubbleSort(array);
+        System.out.println("Bubble Sorted");
+        System.out.println(Arrays.toString(bubbleSorted));
+        int[] selectionSorted = Algorithms.selectionSort(array);
+        System.out.println("Selection Sorted");
+        System.out.println(Arrays.toString(selectionSorted));
+
     }
 }
