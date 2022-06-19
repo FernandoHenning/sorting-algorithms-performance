b0ed220b7c273251b0922d43f51d38cbc0179cdf
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-06-18 19:07:54 -0500

    Created: Selection sort algorithm.

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -22,4 +22,22 @@ public class Algorithms {
         }
         return array;
     }
+
+    static int[] selectionSort(int[] array){
+        int size = array.length;
+        for (int i = 0; i < (size - 1); i++) {
+            int min_index = i;
+            for (int j = i + 1; j < size ; j++) {
+                if(array[j] < array[min_index]){
+                    min_index += 1;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+        }
+        return array;
+    }
+
+
 }
