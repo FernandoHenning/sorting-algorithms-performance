4759bbd11b75bf4b117c3c761b59e9522be4715b
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-06-19 22:42:29 -0500

    Created: Test Cases, DualPivot Quick Sort and DuaPivotPartition methods

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -68,4 +68,48 @@ public class Algorithms {
         swap(array, low, j);
         return j;
     }
+
+    public static void dualPivotQuickSort(int low, int high, int[] array){
+        if(low<high){
+            int[] pivots = dualPivotPartition(low, high,array);
+            dualPivotQuickSort(low, pivots[0]-1, array);
+            dualPivotQuickSort(pivots[0]+1, pivots[1]-1, array);
+            dualPivotQuickSort(pivots[1]+1, high,array);
+
+        }
+    }
+
+    private static int[] dualPivotPartition(int low, int high, int[]array){
+        // If value in low is greater than the value in high they are exchanged
+        if (array[low]>array[high]) swap(array, low, high);
+
+        int j = low + 1;
+        int g = high - 1;
+        int k = low + 1;
+        int leftPivot = array[low];
+        int rightPivot = array[high];
+
+        while (k<=g){
+            if (array[k]< leftPivot){
+                swap(array, k, j);
+                j++;
+
+            } else if (array[k]>=rightPivot) {
+                while (array[g]>rightPivot && k<g) g--;
+                swap(array, k,g);
+                g--;
+                if (array[k]<leftPivot){
+                    swap(array, k,j);
+                    j++;
+                }
+            }
+            k++;
+        }
+        j--;
+        g++;
+        swap(array, low, j);
+        swap(array, high, g);
+
+        return new int[] { j, g };
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,96 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[]{0,6,2,4,1,3,7,8};
+
+        int[] array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
         int size = array.length;
+        long reference=System.nanoTime();
+        Algorithms.dualPivotQuickSort(0, size-1, array);
+        long finishm=System.nanoTime();
+        System.out.println( "Dual Pivot QuickSort [100 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+
+        reference=System.nanoTime();
         Algorithms.quickSort(0, size-1, array);
-        System.out.println(Arrays.toString(array));
+        finishm=System.nanoTime();
+        System.out.println( "QuickSort [100 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+        reference=System.nanoTime();
+        Algorithms.bubbleSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Bubble Sort [100 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+        reference=System.nanoTime();
+        Algorithms.selectionSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Selection Sort [100 elements]:  "+( (double)(finishm-reference) )/1000000000.0);
+
+        // ######################### THOUSAND ELEMENTS ###################################################
+        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
+        size = array.length;
+        reference=System.nanoTime();
+        Algorithms.dualPivotQuickSort(0, size-1, array);
+        finishm=System.nanoTime();
+        System.out.println( "Dual Pivot QuickSort [1,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.quickSort(0, size-1, array);
+        finishm=System.nanoTime();
+        System.out.println( "QuickSort [1,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.bubbleSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Bubble Sort [1,000 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.selectionSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Selection Sort [1,000 elements]:  "+( (double)(finishm-reference) )/1000000000.0);
+
+        // ######################### TEN THOUSAND ELEMENTS ###################################################
+        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
+        size = array.length;
+        reference=System.nanoTime();
+        Algorithms.dualPivotQuickSort(0, size-1, array);
+        finishm=System.nanoTime();
+        System.out.println( "Dual Pivot QuickSort [10,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.quickSort(0, size-1, array);
+        finishm=System.nanoTime();
+        System.out.println( "QuickSort [10,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.bubbleSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Bubble Sort [10,000 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
+
+        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
+
+        reference=System.nanoTime();
+        Algorithms.selectionSort(array);
+        finishm=System.nanoTime();
+        System.out.println( "Selection Sort [10,000 elements]:  "+( (double)(finishm-reference) )/1000000000.0);
+
+
 
     }
 }
