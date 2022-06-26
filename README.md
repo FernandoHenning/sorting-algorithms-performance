b94181ac07e8bd4dbb747e5f851d25d687b622fa
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-06-25 22:32:29 -0500

    New it use the same array to test the same category.

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 In this project I just wanted to test some sorting algorithms that are of interest to me.
 The purpose of this is to compare the execution times of each algorithm with a list of random numbers with different sizes.
 
-Now, I present the results together with the theoretical complexity using the  big(O) notation and a short explanation of them
+Now, I present the results together with the theoretical complexity using the notation big(O) and a short explanation of them
 ## Dual Pivot Quicksort
 Dual pivot quick sort is a little bit faster than the original single pivot quicksort. The idea of dual pivot  is to take two pivots, one in the left end of the array and the second, in the right end of the array. The left pivot must be less than or equal to the right pivot, so we swap them if necessary.
 ### Big(O) complexity = O(n^2)
 ### Execution times
-- Execution time with 100 elements = `7.966E-4` seconds.
-- Execution time with 1,000 elements = `3.285E-4` seconds.
-- Execution time with 10,000 elements = `0.001020201` seconds.
+- Execution time with 100 elements = ` 8.686E-4` seconds.
+- Execution time with 1,000 elements = `3.191E-4` seconds.
+- Execution time with 10,000 elements = `8.274E-4` seconds.
 ---
 ## Quicksort
 Quicksort is based on the divide and conquer approach where an array is divided into subarrays by selecting a pivot element.  
@@ -19,34 +19,33 @@ The left and right subarrays are also divided using the same approach. This proc
 At this point, elements are already sorted. Finally, elements are combined to form a sorted array.
 ### Big(O) complexity = O(n^2)
 ### Execution times
-- Execution time with 100 elements = `2.8201E-5` seconds.
-- Execution time with 1,000 elements = `3.127E-4` seconds.
-- Execution time with 10,000 elements = `0.0010133` seconds.
+- Execution time with 100 elements = `5.01E-5` seconds.
+- Execution time with 1,000 elements = `3.709E-4` seconds.
+- Execution time with 10,000 elements = `0.0341278` seconds.
 ---
  ## Bubble Sort
 Bubble sort is a sorting algorithm that compares two adjacent elements and swaps them until they are not in the intended order.  
 ### Big(O) complexity = O(n^2)
 ### Execution times
-- Execution time with 100 elements = `1.931E-4` seconds.
-- Execution time with 1,000 elements = `0.0045168` seconds.
-- Execution time with 10,000 elements = `0.0877598` seconds.
+- Execution time with 100 elements = `1.809E-4` seconds.
+- Execution time with 1,000 elements = `0.0047738` seconds.
+- Execution time with 10,000 elements = `1.42E-4` seconds.
 ---
 ## Selection sort
 Selection sort is a sorting algorithm that selects the smallest element from an unsorted list in each iteration and places that element at the beginning of the unsorted list.
 ### Big(O) complexity = O(n^2)
 ### Execution times
-- Execution time with 100 elements = `1.16E-4` seconds.
-- Execution time with 1,000 elements = `0.0025865` seconds.
-- Execution time with 10,000 elements = `0.0629375` seconds.
+- Execution time with 100 elements = `8.37E-5` seconds.
+- Execution time with 1,000 elements = `0.0023662` seconds.
+- Execution time with 10,000 elements = ` 0.0470242` seconds.
 ---
 ## Conclusion
 According to the results obtained by my computer these are the best algorithms based on the number of elements.  
 
-| Number of elements | Algorithm | Time |
-|--------------------|-----------|------|
-| 100                | Quicksort |  2.8201E-5    |
-| 1,000              | Quicksort |3.127E-4     |
-| 10,000             | Quicksort |    0.0010133  |  
-
+| Number of elements | Algorithm            | Time |
+|--------------------|----------------------|------|
+| 100                | Quicksort            |5.01E-5|
+| 1,000              | Dual Pivot Quicksort |3.191E-4 |
+| 10,000             | ¿Bubblesort?         |1.42E-4 |
 As we can see the fastest algorithm was Quicksort. Curious, since, in theory, Dual Pivot Quicksort is a bit faster.  
 Performing multiple runs I obtained different results, but the top positions were always between Quicksort and Dual Pivot Quicksort.
--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,34 @@ public class Main {
         int[] array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
         int size = array.length;
         long reference=System.nanoTime();
-        Algorithms.dualPivotQuickSort(0, size-1, array);
+
+        Algorithms.dualPivotQuickSort(0, size-1, Arrays.copyOf(array, array.length));
+
         long finishm=System.nanoTime();
         System.out.println( "Dual Pivot QuickSort [100 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+
 
         reference=System.nanoTime();
-        Algorithms.quickSort(0, size-1, array);
+
+        Algorithms.quickSort(0, size-1, Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "QuickSort [100 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+
         reference=System.nanoTime();
-        Algorithms.bubbleSort(array);
+
+        Algorithms.bubbleSort(Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Bubble Sort [100 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+
         reference=System.nanoTime();
-        Algorithms.selectionSort(array);
+
+        Algorithms.selectionSort(Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Selection Sort [100 elements]:  "+( (double)(finishm-reference) )/1000000000.0 + "\n");
 
@@ -36,28 +44,31 @@ public class Main {
         array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
         size = array.length;
         reference=System.nanoTime();
-        Algorithms.dualPivotQuickSort(0, size-1, array);
+
+        Algorithms.dualPivotQuickSort(0, size-1, Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Dual Pivot QuickSort [1,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.quickSort(0, size-1, array);
+
+        Algorithms.quickSort(0, size-1, Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "QuickSort [1,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.bubbleSort(array);
+        Algorithms.bubbleSort(Arrays.copyOf(array, array.length));
         finishm=System.nanoTime();
         System.out.println( "Bubble Sort [1,000 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(1000)).limit(1000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.selectionSort(array);
+
+        Algorithms.selectionSort(Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Selection Sort [1,000 elements]:  "+( (double)(finishm-reference) )/1000000000.0 + "\n");
 
@@ -65,28 +76,33 @@ public class Main {
         array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
         size = array.length;
         reference=System.nanoTime();
+
         Algorithms.dualPivotQuickSort(0, size-1, array);
+
         finishm=System.nanoTime();
         System.out.println( "Dual Pivot QuickSort [10,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.quickSort(0, size-1, array);
+
+        Algorithms.quickSort(0, size-1, Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "QuickSort [10,000 elements]: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.bubbleSort(array);
+
+        Algorithms.bubbleSort(Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Bubble Sort [10,000 elements]:: "+( (double)(finishm-reference) )/1000000000.0);
 
-        array = IntStream.generate(() -> new Random().nextInt(10_000)).limit(10_000).toArray();
 
         reference=System.nanoTime();
-        Algorithms.selectionSort(array);
+
+        Algorithms.selectionSort(Arrays.copyOf(array, array.length));
+
         finishm=System.nanoTime();
         System.out.println( "Selection Sort [10,000 elements]:  "+( (double)(finishm-reference) )/1000000000.0);
 
